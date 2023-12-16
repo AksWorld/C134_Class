940d6ec4df7b43a3739a533a94e271739482596c
AksWorld <123278691+AksWorld@users.noreply.github.com>
2023-12-15 19:59:40 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+img="";
+Status="";
+objects=[];
+function preload(){
+   img=loadImage('dog_cat.jpg') 
+}
+function setup(){
+    canvas=createCanvas(380,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detected Objects";
+}
+function modelLoaded(){
+    console.log("Tis the season to be jolly fa la la la  la la la la LAAAAAAAAAA!!!");
+    Status=true;
+    objectDetector.detect(video,gotResult)
+}
+function draw(){
+  image(video,0,0,380,380);
+  if(Status != ""){
+    r=random(255);
+    g=random(225);
+    b= random(255);
+    objectDetector.detect(video,gotResult)
+    for(i=0; i< objects.length; i++){
+       document.getElementById("status").innerHTML="Status : Object Detected";
+       document.getElementById("number_of_objects").innerHTML="Number of objects detected are : " + objects.length;
+       fill(r,g,b);
+       percent=floor(objects[i].confidence*100);
+       text(objects[i].label + " " + percent + "%",objects[i].x + 15,objects[i].y + 15);
+       noFill();
+       stroke(r,g,b);
+       rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+  }
+}
+function gotResult(error,results){
+  if(error){
+    console.log(error);
+  }
+  console.log(results);
+  objects=results;
+}
